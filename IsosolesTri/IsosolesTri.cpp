71cca20a1550d46244686c8e7c3c9000733ae199
adr0179 <143733322+adr0179@users.noreply.github.com>
2025-09-20 13:02:43 -0400

    add reverse triangle function

--- IsosolesTri/IsosolesTri.cpp
+++ IsosolesTri/IsosolesTri.cpp
@@ -2,7 +2,7 @@
 #include<string>
 using namespace std;
 
-void IsosolesTri(int length) {
+void isosolesTri(int length) {
     for (int i = 0; i < length; i++) {
         for (int j = length - 1; j > i; j--) {
             cout << " ";
@@ -16,6 +16,21 @@ void IsosolesTri(int length) {
     }
 }
 
+void invertedTri(int length) {
+    for (int i = length; i > 0; i--) {
+        for (int j = length; j > i; j--) {
+            cout << " ";
+        }
+
+        for (int k = 0; k < 2*i - 1; k++) {
+            cout << i;
+        }
+
+        cout << endl;
+    }
+}
+
 int main() {
-    IsosolesTri(5);
+    isosolesTri(5);
+    invertedTri(5);
 }
