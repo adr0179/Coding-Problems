237764cc69908b680dcb7b04896516814d177c88
Adrian <adrianahmadi06@gmail.com>
2025-10-01 08:44:31 -0400

    factorial for loop implementation

---
+++ GeneralCodingProblems/Factorial/Fact.java
@@ -0,0 +1,24 @@
+package Factorial;
+
+public class Fact {
+    public int factorial(int val) {
+        int res = val;
+
+        if (val == 0) {
+            return 1;
+        }
+
+        for (int i = val - 1; i > 0; i--) {
+            res *= i;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Fact f = new Fact();
+
+        System.out.println(f.factorial(5));
+        System.out.println(f.factorial(0));
+    }
+}
